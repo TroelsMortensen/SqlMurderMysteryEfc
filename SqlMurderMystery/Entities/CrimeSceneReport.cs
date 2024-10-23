a90bb177b9e16d9f5097c6a305716a99dc14a247
TRMO <tromortensen@hotmail.com>
2024-10-23 11:45:28 +0200

    add crime scene report class

---
+++ SqlMurderMystery/Entities/CrimeSceneReport.cs
@@ -0,0 +1,14 @@
+﻿namespace SqlMurderMystery.Entities;
+
+public class CrimeSceneReport
+{
+    public int Id { get; set; }
+    
+    public int Date { get; set; }
+
+    public string Type { get; set; }
+
+    public string? Description { get; set; }
+
+    public string City { get; set; }
+}
