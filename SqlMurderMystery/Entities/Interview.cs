b27d4078b22859357a27458fabd40d3c09a52aee
TRMO <tromortensen@hotmail.com>
2024-10-23 11:47:12 +0200

    add interview class

---
+++ SqlMurderMystery/Entities/Interview.cs
@@ -0,0 +1,10 @@
+﻿namespace SqlMurderMystery.Entities;
+
+public partial class Interview
+{
+    public int? PersonId { get; set; }
+
+    public string? Transcript { get; set; }
+
+    public virtual Person? Person { get; set; }
+}
