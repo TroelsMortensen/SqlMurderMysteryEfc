d15c0e39b7a6c9df7da22f8ae97062ac59d7daba
TRMO <tromortensen@hotmail.com>
2024-10-23 14:46:31 +0200

    include example query

--- SqlMurderMystery/Queries.cs
+++ SqlMurderMystery/Queries.cs
@@ -1,11 +1,21 @@
+using Xunit.Abstractions;
+
 namespace SqlMurderMystery;
 
 public class Queries
 {
+    private readonly ITestOutputHelper outPutter;
     private AppContext ctx = new();
-    
-    [Fact]
-    public void Test1()
+
+    public Queries(ITestOutputHelper outPutter)
     {
+        this.outPutter = outPutter;
+    }
+
+    [Fact]
+    public void Print10People()
+    {
+        var persons = ctx.People.Take(10).ToList();
+        outPutter.PrintList(persons);
     }
 }
