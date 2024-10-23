f7e889afa943bb379c98325152cb79ccc1c5e520
TRMO <tromortensen@hotmail.com>
2024-10-23 11:54:38 +0200

    add get fit now member class

---
+++ SqlMurderMystery/Entities/GetFitNowMember.cs
@@ -0,0 +1,16 @@
+﻿namespace SqlMurderMystery.Entities;
+
+public partial class GetFitNowMember
+{
+    public string Id { get; set; } = null!;
+
+    public int? PersonId { get; set; }
+
+    public string? Name { get; set; }
+
+    public int? MembershipStartDate { get; set; }
+
+    public string? MembershipStatus { get; set; }
+
+    public virtual Person? Person { get; set; }
+}
