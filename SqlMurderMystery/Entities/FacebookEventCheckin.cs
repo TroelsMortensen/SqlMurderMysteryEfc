a46032fb4d0d5de6c6b35b06edeef607d0068541
TRMO <tromortensen@hotmail.com>
2024-10-23 11:50:08 +0200

    adding facebook event checkin class

---
+++ SqlMurderMystery/Entities/FacebookEventCheckin.cs
@@ -0,0 +1,14 @@
+﻿namespace SqlMurderMystery.Entities;
+
+public partial class FacebookEventCheckin
+{
+    public int? PersonId { get; set; }
+
+    public int? EventId { get; set; }
+
+    public string? EventName { get; set; }
+
+    public int? Date { get; set; }
+
+    public virtual Person? Person { get; set; }
+}
